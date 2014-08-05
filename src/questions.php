1b358b15a753165e5bc3779cd5d62c1288fbf52c
KeironO <keo7@aber.ac.uk>
2014-08-05 06:57:26 +0200

    Add identifier

--- src/questions.php
+++ src/questions.php
@@ -2,8 +2,9 @@
 
 require("lib.php");
 
-function print_question($module, $question_text, $type) {
+function print_question($identifier, $question_text, $type) {
 	echo "<h3>{$question_text}</h3>";
+	echo $identifier;
 }
 
 $questions = getQuestions();
@@ -12,12 +13,14 @@ $modules = getStudentModules('keo7');
 foreach($modules as $module) {
 	echo "<h2>{$module["ModuleTitle"]}</h2>";
 	foreach($questions as $question) {
+		$identifier = "{$module["ModuleID"]}_{$question["QuestionID"]}";
 		if ($question["Staff"] == 0) {
-			print_question($module, $question["QuestionText"], "rate");
+			print_question($identifier, $question["QuestionText"], "rate");
 		}
 		else {
 			foreach($module["Staff"] as $staff) {
-				print_question($module, sprintf($question["QuestionText"], $staff["StaffName"]), "rate");
+				$staff_identifier = "{$identifier}_{$staff["StaffID"]}";
+				print_question($staff_identifier, sprintf($question["QuestionText"], $staff["StaffName"]), "rate");
 			}
 		}
 	}
