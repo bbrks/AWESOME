57ac84f2d2e1296b8554019a361c930ed65f0470
KeironO <keo7@aber.ac.uk>
2014-08-10 01:55:50 +0100

    Add questionaire

--- src/admin/home.php
+++ src/admin/home.php
@@ -5,6 +5,9 @@
 	<link rel="icon" type="image/png" href="../../assets/favicon.png">
 
 	<link href="../css/bootstrap.min.css" rel="stylesheet">
+	<script src="../js/jquery-1.11.0.min.js" type="text/javascript"></script>
+	<script src="../js/bootstrap.min.js" type="text/javascript"></script>
+	
 	
 </head>
 
@@ -19,7 +22,42 @@ if (!isset($_SESSION["admin_user"])) {
 	header("location: login.php");
 	exit("login ffs");
 }
+?>
 
+<div class="modal fade" id="modal">
+  <div class="modal-dialog">
+    <div class="modal-content">
+	  <form method="post" action="__addquestionaire.php">
+		  <div class="modal-header">
+			<button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+			<h4 class="modal-title">Add Questionaire</h4>
+		  </div>
+		  <div class="modal-body">
+			<p>To begin with, just fill in the name, and department</p>
+			<label for="questionaireName">Name</label>
+			<input type="text" name="questionaireName" class="form-control" />
+			<label for="questionaireDepartment">Department</label>
+			<select name="questionaireDepartment" class="form-control">
+				<option value="Art">Art</option>
+				<option value="IBERS">IBERS</option>
+				<option value="CompSci">CompSci</option>
+				<option value="Welsh">Walsh</option>
+			</select>
+			
+		  </div>
+		  <div class="modal-footer">
+			<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+			<button type="submit" class="btn btn-primary">Add Questionaire</button>
+		  </div>
+      </form>
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+
+
+
+
+<? //TABLE
 require "lib-admin.php";
 $questionaires = getQuestionaires();
 ?>
@@ -32,7 +70,7 @@ $questionaires = getQuestionaires();
 		<th>Answers</th>
 		<th>Total Students</th>
 		<th class="col-sm-4">Percent</th>
-		<td><button type="button" class="btn btn-primary ">Add</button></td>
+		<td><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modal">Add</button></td>
 	</thead>
 	<?
 	
@@ -53,3 +91,6 @@ foreach($questionaires as $questionaire) {
 	
 	?>
 </table>
+
+</body>
+</html>
