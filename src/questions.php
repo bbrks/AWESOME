984f644707712f8d5ff93699dd43ffef00b88629
KeironO <keo7@aber.ac.uk>
2014-08-14 03:33:10 +0100

    Improved radio buttons + textareas

--- src/questions.php
+++ src/questions.php
@@ -3,10 +3,50 @@
 <head>
 	<title>Questionnaire</title>
 	<link rel="icon" type="image/png" href="../../assets/favicon.png">
-
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link href="css/bootstrap.min.css" rel="stylesheet">
-
+	<link href="css/prettyCheckable.css" rel="stylesheet">
+	
+	<style>
+		.ratetable {
+			border-spacing: 5px 0px;
+			border-collapse: separate;
+		}
+		
+		.prettyradio {
+			text-align: center;
+		}
+		
+		.prettyradio a {
+			float: none;
+			margin: auto;
+		}
+		
+		.prettyradio label {
+			float: none;
+			
+			text-align: center;
+			width: 100%;
+			margin: auto;
+		}
+		
+	</style>
+	
+	<script src="js/jquery-1.11.0.min.js" type="text/javascript"></script>
+	<script src="js/bootstrap.min.js" type="text/javascript"></script>
+	<script src="js/prettyCheckable.min.js" type="text/javascript"></script>
+	<script type="text/javascript">
+		$(function() {
+			$(".ratetable input[type=radio]").each(function(e) {
+					$(this).prettyCheckable({
+					"customClass": "prettyrate",
+					"labelPosition": "left"
+				});
+			});
+		})
+	</script>
 </head>
+
 
 <body>
 
@@ -34,30 +74,30 @@ function print_question($question, $warn=false) {
 
 	if ($question["Type"] == "rate") {
 		echo "<hr>
-		<table>
+		<table class=\"ratetable\">
 			<thead>
 				<th></th>
-				<th>1</th>
-				<th>2</th>
-				<th>3</th>
-				<th>4</th>
-				<th>5</th>
+				<th><label for=\"{$question["Identifier"]}_1\">1</label></th>
+				<th><label for=\"{$question["Identifier"]}_2\">2</label></th>
+				<th><label for=\"{$question["Identifier"]}_3\">3</label></th>
+				<th><label for=\"{$question["Identifier"]}_4\">4</label></th>
+				<th><label for=\"{$question["Identifier"]}_5\">5</label></th>
 				<th></th>
 			</thead>
 			<tr>
 				<td>Strongly Disagree</td>
-				<td><input type=\"radio\" name=\"{$question["Identifier"]}\" value=\"1\" ". ($question["Answer"]==1?"checked=\"true\"":"") ."></td>
-				<td><input type=\"radio\" name=\"{$question["Identifier"]}\" value=\"2\" ". ($question["Answer"]==2?"checked=\"true\"":"") ."></td>
-				<td><input type=\"radio\" name=\"{$question["Identifier"]}\" value=\"3\" ". ($question["Answer"]==3?"checked=\"true\"":"") ."></td>
-				<td><input type=\"radio\" name=\"{$question["Identifier"]}\" value=\"4\" ". ($question["Answer"]==4?"checked=\"true\"":"") ."></td>
-				<td><input type=\"radio\" name=\"{$question["Identifier"]}\" value=\"5\" ". ($question["Answer"]==5?"checked=\"true\"":"") ."></td>
+				<td><input type=\"radio\" name=\"{$question["Identifier"]}\" id=\"{$question["Identifier"]}_1\" value=\"1\" ". ($question["Answer"]==1?"checked=\"true\"":"") ."></td>
+				<td><input type=\"radio\" name=\"{$question["Identifier"]}\" id=\"{$question["Identifier"]}_2\" value=\"2\" ". ($question["Answer"]==2?"checked=\"true\"":"") ."></td>
+				<td><input type=\"radio\" name=\"{$question["Identifier"]}\" id=\"{$question["Identifier"]}_3\" value=\"3\" ". ($question["Answer"]==3?"checked=\"true\"":"") ."></td>
+				<td><input type=\"radio\" name=\"{$question["Identifier"]}\" id=\"{$question["Identifier"]}_4\" value=\"4\" ". ($question["Answer"]==4?"checked=\"true\"":"") ."></td>
+				<td><input type=\"radio\" name=\"{$question["Identifier"]}\" id=\"{$question["Identifier"]}_5\" value=\"5\" ". ($question["Answer"]==5?"checked=\"true\"":"") ."></td>
 				<td>Strongly Agree</td>
 			</tr>
 		</table>
 		<hr>";
 	}
 	elseif ($question["Type"] == "text") {
-		echo "<textarea name=\"{$question["Identifier"]}\" rows=\"8\" cols=\"50\">{$question["Answer"]}</textarea>";
+		echo "<textarea name=\"{$question["Identifier"]}\" rows=\"8\" cols=\"50\" class=\"form-control\">{$question["Answer"]}</textarea>";
 	}
 	echo "</div>";
 }
