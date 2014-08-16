15ce3b8c05a6ae103e91147eadf9cba7e99bcdbe
KeironO <keo7@aber.ac.uk>
2014-08-16 04:23:02 +0100

    Make fake default to 0 (make it compatible with existing insert code)

--- src/update.php
+++ src/update.php
@@ -52,7 +52,7 @@ if ($version == 2) {
 
 if ($version == 3) {
 	$stmt = new tidy_sql($db, "ALTER TABLE `Modules`
-	ADD `Fake` bit NOT NULL,
+	ADD `Fake` bit NOT NULL DEFAULT 0,
 	COMMENT='';")
 	$stmt->query();
 	
