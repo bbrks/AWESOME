eead7bf3ee577db4b2c018e0f9d193fcd282bd65
KeironO <keo7@aber.ac.uk>
2014-08-17 22:39:52 +0000

    Implement students select

--- src/admin/modify/students.php
+++ src/admin/modify/students.php
@@ -5,7 +5,20 @@ Twig_Autoloader::register();
 $loader = new Twig_Loader_Filesystem("{$root}/admin/tpl/");
 $twig = new Twig_Environment($loader, array());
 
-
 $template = $twig->loadTemplate('students.html');
-echo $template->render(array("url"=>$url, "title"=>"test","content"=>"hello world!"));
+
+$questionaireID = $_GET["questionaireID"];
+
+$stmt = new tidy_sql($db, "
+	SELECT Students.UserID, Students.Department, Students.Token, GROUP_CONCAT(DISTINCT ModuleID ORDER BY ModuleID ASC SEPARATOR ' ') AS Modules, Students.Done
+	FROM Students
+	JOIN StudentsToModules ON StudentsToModules.UserID=Students.UserID AND StudentsToModules.QuestionaireID=Students.QuestionaireID 
+	WHERE Students.QuestionaireID=?
+	GROUP BY Students.UserID
+	ORDER BY Students.Done DESC
+", "i");
+
+$rows = $stmt->query($questionaireID);
+
+echo $template->render(array("url"=>$url, "title"=>"test","students"=>$rows));
 
