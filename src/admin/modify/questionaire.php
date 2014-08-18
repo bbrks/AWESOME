922ad46809b33af1092ba029b29cfca3f7e41676
KeironO <keo7@aber.ac.uk>
2014-08-18 05:00:12 +0000

    New main questionnaire page

---
+++ src/admin/modify/questionaire.php
@@ -0,0 +1,16 @@
+<?
+require "../../lib.php";
+require_once "{$root}/lib/Twig/Autoloader.php";
+
+Twig_Autoloader::register();
+$loader = new Twig_Loader_Filesystem("{$root}/admin/tpl/");
+$twig = new Twig_Environment($loader, array());
+
+$template = $twig->loadTemplate('questionaire.html');
+
+$questionaireID = $_GET["questionaireID"];
+$alerts = array();
+
+echo $template->render(array(
+	"url"=>$url, "questionaireID"=> $questionaireID, "alerts"=>$alerts,
+));
