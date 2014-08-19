57803c5e447f69d2fd0ea01332ab75042d0680c7
KeironO <keo7@aber.ac.uk>
2014-08-19 21:22:03 +0000

    global add question support

--- src/admin/questionaire/customise/module.php
+++ src/admin/questionaire/customise/module.php
@@ -9,8 +9,38 @@ $twig = new Twig_Environment($loader, array());
 $template = $twig->loadTemplate('questionaire/customise/module.html');
 
 $questionaireID = $_GET["questionaireID"];
-$moduleID = $_GET["moduleID"];
+$moduleID = isset($_GET["moduleID"])?$_GET["moduleID"]:null;
 $alerts = array();
+
+function getModule() {
+	global $questionaireID, $moduleID, $alerts, $db;
+	if (!$moduleID) {
+		return array("ModuleID"=>"global", "ModuleTitle"=>"Repeated questions", "Fake"=>true);
+	}
+	else {
+		$stmt = new tidy_sql($db, "
+			SELECT ModuleID, ModuleTitle, Fake FROM Modules WHERE QuestionaireID=? AND ModuleID=?
+		", "is");
+		$module = $stmt->query($questionaireID, $moduleID);
+		return $module[0];
+	}
+}
+
+function getModuleQuestions() { //SQL's WHERE is fussy
+	global $questionaireID, $moduleID, $alerts, $db;
+	if (!$moduleID) {
+		$stmt = new tidy_sql($db, "
+			SELECT QuestionID, QuestionText, QuestionText_welsh, Type FROM Questions WHERE QuestionaireID=? AND ModuleID is NULL
+		", "i");
+		return $stmt->query($questionaireID);
+	}
+	else {
+		$stmt = new tidy_sql($db, "
+			SELECT QuestionID, QuestionText, QuestionText_welsh, Type FROM Questions WHERE QuestionaireID=? AND ModuleID=?
+		", "is");
+		return $stmt->query($questionaireID, $moduleID);
+	}
+}
 
 function insertQuestion($details) {
 	global $questionaireID, $moduleID, $alerts, $db;
@@ -31,18 +61,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 	}
 }
 
-$stmt = new tidy_sql($db, "
-	SELECT QuestionID, QuestionText, QuestionText_welsh, Type FROM Questions WHERE QuestionaireID=? AND ModuleID=?
-", "is");
-$questions = $stmt->query($questionaireID, $moduleID);
-
-$stmt = new tidy_sql($db, "
-	SELECT ModuleID, ModuleTitle, Fake FROM Modules WHERE QuestionaireID=? AND ModuleID=?
-", "is");
-$module = $stmt->query($questionaireID, $moduleID);
+$module = getModule();
+$questions = getModuleQuestions();
 
 echo $template->render(array(
 	"url"=>$url, "questionaireID"=> $questionaireID, "alerts"=>$alerts,
 	"questions"=>$questions,
-	"module"=>$module[0]
+	"module"=>$module
 ));
