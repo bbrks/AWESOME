668e4a6ebf93b0786ce1acba89bf249f439e624a
KeironO <keo7@aber.ac.uk>
2014-08-31 03:37:41 +0000

    Port over result module list to template

--- src/admin/questionaire/results/modules.php
+++ src/admin/questionaire/results/modules.php
@@ -1,62 +1,28 @@
-<?php
-session_start();
+<?
+require "../../../lib.php";
+require_once "{$root}/lib/Twig/Autoloader.php";
 
-if (!isset($_SESSION["admin_user"])) {
-	header("location: login.php");
-	exit("login ffs");
-}
-require "../../lib-admin.php";
+Twig_Autoloader::register();
+$loader = new Twig_Loader_Filesystem("{$root}/admin/tpl/");
+$twig = new Twig_Environment($loader, array());
+
+$template = $twig->loadTemplate('questionaire/results/modules.html');
 
 $questionaireID = $_GET["questionaireID"];
+$alerts = array();
 
-?>
-<!DOCTYPE HTML>
-<html>
-<head>
-	<title>Questionnaire</title>
-	<link rel="icon" type="image/png" href="../../assets/favicon.png">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link href="../css/bootstrap.min.css" rel="stylesheet">
-	<script src="../js/jquery-1.11.0.min.js" type="text/javascript"></script>
-	<script src="../js/bootstrap.min.js" type="text/javascript"></script>
-	<script src="../js/Chart.min.js" type="text/javascript"></script>
-	<style>
-		.table .progress {
-			margin-bottom: 0px;
-		}
-	</style>
-	
-</head>
 
-<body>
-<?
-	$stmt = new tidy_sql($db, "
-		SELECT Modules.ModuleID,Modules.ModuleTitle,COUNT(DISTINCT AnswerGroup.AnswerID) as NumAnswers FROM Modules
-		JOIN StudentsToModules ON StudentsToModules.ModuleID = Modules.ModuleID AND StudentsToModules.QuestionaireID = Modules.QuestionaireID
-		LEFT JOIN Answers ON Answers.ModuleID = Modules.ModuleID
-		LEFT JOIN AnswerGroup ON AnswerGroup.QuestionaireID = Modules.QuestionaireID
-		WHERE Modules.QuestionaireID=?
-		GROUP BY Modules.ModuleID
-		", "i");
-	$rows = $stmt->query($questionaireID);
-	?>
-	<table class="table">
-		<thead>
-			<td>Module ID</td>
-			<th>Module Title</th>
-			<th>Num Answers</th>
-		</thead>
-	<?
-	foreach($rows as $row) {
-		?>
-		<tr>
-			<td><?=$row["ModuleID"]?></td>
-			<td><a href="moduleresults.php?questionaireID=<?=$questionaireID?>&moduleID=<?=$row["ModuleID"]?>"><?=$row["ModuleTitle"]?></a></td>
-			<td><?=$row["NumAnswers"]?></td>
-		</tr>
-		<?
-	}
-	?>
-	</table>
-</body>
-</html>
+$stmt = new tidy_sql($db, "
+	SELECT Modules.ModuleID,Modules.ModuleTitle,COUNT(DISTINCT AnswerGroup.AnswerID) as NumAnswers FROM Modules
+	JOIN StudentsToModules ON StudentsToModules.ModuleID = Modules.ModuleID AND StudentsToModules.QuestionaireID = Modules.QuestionaireID
+	LEFT JOIN Answers ON Answers.ModuleID = Modules.ModuleID
+	LEFT JOIN AnswerGroup ON AnswerGroup.QuestionaireID = Modules.QuestionaireID
+	WHERE Modules.QuestionaireID=?
+	GROUP BY Modules.ModuleID
+	", "i");
+$modules = $stmt->query($questionaireID);
+
+echo $template->render(array(
+	"url"=>$url, "questionaireID"=> $questionaireID, "alerts"=>$alerts,
+	"modules"=>$modules
+));
