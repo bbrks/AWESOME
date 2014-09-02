d52ffb00868c85bcb7f83fc196b1bccd0df3cb1a
KeironO <keo7@aber.ac.uk>
2014-09-02 17:40:14 +0100

    Implement fake module percent

--- src/admin/questionaire/results/modules.php
+++ src/admin/questionaire/results/modules.php
@@ -11,30 +11,47 @@ $template = $twig->loadTemplate('questionaire/results/modules.html');
 $questionaireID = $_GET["questionaireID"];
 $alerts = array();
 
+function getModulesList($questionaireID) {
+	global $db;
+	
+	$stmt = new tidy_sql($db, "
+	SELECT Modules.ModuleID,Modules.ModuleTitle,(
+		SELECT COUNT(DISTINCT Answers.AnswerID)
+		FROM Answers
+		JOIN AnswerGroup ON Answers.AnswerID=AnswerGroup.AnswerID
+		WHERE Answers.ModuleID=Modules.ModuleID AND
+		AnswerGroup.QuestionaireID=Modules.QuestionaireID
+		) as NumAnswers, (
+			SELECT COUNT(*)
+			FROM StudentsToModules
+			WHERE StudentsToModules.QuestionaireID=Modules.QuestionaireID AND
+			StudentsToModules.ModuleID=Modules.ModuleID
+		) as TotalStudents,
+		Fake
+	FROM Modules
+	LEFT JOIN StudentsToModules ON StudentsToModules.QuestionaireID=Modules.QuestionaireID AND StudentsToModules.ModuleID=Modules.ModuleID
+	WHERE Modules.QuestionaireID=? AND (Modules.Fake = true OR StudentsToModules.ModuleID is not null)
+	GROUP BY Modules.ModuleID
+	ORDER BY Modules.Fake DESC,NumAnswers/TotalStudents DESC,NumAnswers DESC
+		", "i");
+		
+	$modules = $stmt->query($questionaireID);
+	return $modules;
+}
 
-$stmt = new tidy_sql($db, "
-SELECT Modules.ModuleID,Modules.ModuleTitle,(
-	SELECT COUNT(DISTINCT Answers.AnswerID)
-	FROM Answers
-	JOIN AnswerGroup ON Answers.AnswerID=AnswerGroup.AnswerID
-	WHERE Answers.ModuleID=Modules.ModuleID AND
-	AnswerGroup.QuestionaireID=Modules.QuestionaireID
-	) as NumAnswers, (
-		SELECT COUNT(*)
-		FROM StudentsToModules
-		WHERE StudentsToModules.QuestionaireID=Modules.QuestionaireID AND
-		StudentsToModules.ModuleID=Modules.ModuleID
-	) as TotalStudents,
-	Fake
-FROM Modules
-LEFT JOIN StudentsToModules ON StudentsToModules.QuestionaireID=Modules.QuestionaireID AND StudentsToModules.ModuleID=Modules.ModuleID
-WHERE Modules.QuestionaireID=? AND (Modules.Fake = true OR StudentsToModules.ModuleID is not null)
-GROUP BY Modules.ModuleID
-ORDER BY Modules.Fake DESC,NumAnswers/TotalStudents DESC,NumAnswers DESC
-	", "i");
-$modules = $stmt->query($questionaireID);
+function getTotalQuestionnaireStudents($questionaireID) {
+	global $db;
+	
+	$stmt = new tidy_sql($db, "SELECT COUNT(DISTINCT UserID) as total FROM Students WHERE QuestionaireID=?", "i");
+	$info = $stmt->query($questionaireID);
+	return $info[0]["total"];
+}
+
+$modules = getModulesList($questionaireID);
+$totalstudents = getTotalQuestionnaireStudents($questionaireID);
 
 echo $template->render(array(
 	"url"=>$url, "questionaireID"=> $questionaireID, "alerts"=>$alerts,
+	"totalstudents"=>$totalstudents,
 	"modules"=>$modules
 ));
