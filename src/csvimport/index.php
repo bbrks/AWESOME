a80a3265efd178fa1f0663af807526a6fbbb4bcf
Keiron O'Shea <keiron.oshea@gmail.com>
2014-09-07 20:39:07 +0100

    Got rid of shit

--- src/csvimport/index.php
+++
@@ -1,9 +0,0 @@
-<p><a href="uploadcsv.php">Here, stupid</a></p>
-<br>
-<?php
-if (!function_exists('mysqli_init') && !extension_loaded('mysqli')) {
-    echo 'We don\'t have mysqli!!!';
-} else {
-    echo 'Phew we have mysqli!';
-}
-?>
