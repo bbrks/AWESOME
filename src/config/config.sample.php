7313270506f4cf0453efbaf8186696b62724e7f2
Ben Brooks <ben@bbrks.me>
2015-02-13 23:13:25 +0000

    Add config.sample.php

---
+++ src/config/config.sample.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Contains configuration required by the system.
+ *
+ * ATTENTION: Copy/rename this file to config.php before modifying!
+ *
+ * @author Ben Brooks (beb12@aber.ac.uk)
+ * @version 1.1-alpha
+ */
+class Config {
+
+  /**
+   * Constants to store database credentials.
+   * @const DB_USER Database username
+   * @const DB_PASS Database password
+   * @const DB_HOST Database hostname[:port] (Default: 'localhost')
+   * @const DB_NAME Database name (Default: 'awesome')
+   * @const DB_PREFIX Table prefix (Default: 'awe_')
+   */
+  const DB_USER = '';
+  const DB_PASS = '';
+  const DB_HOST = 'localhost';
+  const DB_NAME = 'awesome';
+  const TB_PREFIX = 'awe_';
+
+  /*******************************************************************
+   ** DANGER: DO NOT MODIFY BELOW UNLESS YOU KNOW WHAT YOU'RE DOING **
+   *******************************************************************/
+
+  /**
+   * Boolean to enable production-dangerous features, like display errors.
+   * @const DEBUG
+   */
+  const DEBUG = false;
+
+}
