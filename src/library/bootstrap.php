2de45088b1cb186421fc5378a005c8d1cf66effc
Ben Brooks <ben@bbrks.me>
2015-02-14 01:20:01 +0000

    Add library/boostrap.php

---
+++ src/library/bootstrap.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Define a few useful constants
+ * @const DS Alias for DIRECTORY_SEPARATOR
+ * @const ROOT Relative path to src
+ */
+define('DS', DIRECTORY_SEPARATOR);
+define('ROOT', dirname(dirname(__FILE__)));
+
+/**
+ * Require ./config/config.php and ./library/shared.php
+ */
+require_once(ROOT . DS . 'config' . DS . 'config.php');
+require_once(ROOT . DS . 'library' . DS . 'shared.php');
