f5caebe68da737f6a9ca4628bdd02d435c09b082
Ben Brooks <ben@bbrks.me>
2015-02-18 18:37:33 +0000

    Change class.php suffix to now CamelCase the filenames instead.

--- src/lib/shared.php
+++ src/lib/shared.php
@@ -107,8 +107,8 @@ function callHook($url) {
  * @param string $className
  */
 function __autoload($className) {
-  if (file_exists(ROOT.DS.'lib'.DS.strtolower($className).'.class.php')) {
-    require_once(ROOT.DS .'lib'.DS.strtolower($className).'.class.php');
+  if (file_exists(ROOT.DS.'lib'.DS.strtolower($className).'.php')) {
+    require_once(ROOT.DS .'lib'.DS.strtolower($className).'.php');
   } else if (file_exists(ROOT.DS.'app'.DS.'controllers'.DS.strtolower($className).'.php')) {
     require_once(ROOT.DS.'app'.DS.'controllers'.DS.strtolower($className).'.php');
   } else if (file_exists(ROOT.DS.'app'.DS.'models'.DS.strtolower($className).'.php')) {
