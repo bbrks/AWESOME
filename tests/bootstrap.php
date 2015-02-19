74db545db2735e5f8b4d8ad75e317293e1fb55a5
Ben Brooks <ben@bbrks.me>
2015-02-19 22:30:47 +0000

    Fix unit test config path

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,5 @@
 <?php
 
 require_once(dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'lib' . DIRECTORY_SEPARATOR . 'utils.php');
-require_once(ROOT.DS.'config'.DS.'config.php');
+require_once(ROOT.DS.'config'.DS.'config.sample.php');
 require_once(ROOT.DS.'lib'.DS.'I18n.php');
