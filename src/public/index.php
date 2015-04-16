f2e091ab664ffbd7224e4a0404333e2bff2e8ab7
Ben Brooks <ben@bbrks.me>
2015-04-16 05:02:26 +0100

    Add basic URL format checking

--- src/public/index.php
+++ src/public/index.php
@@ -2,4 +2,8 @@
 
 $url = $_GET['url'];
 
+if (preg_match('/^questionnaires\/view\/[a-z0-9]{16}(\?.*)?$/', $url) == 0) {
+  die('Invalid URL.');
+}
+
 require_once(dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'lib' . DIRECTORY_SEPARATOR . 'bootstrap.php');
