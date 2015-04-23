c4b84bba9ca94d6527b653c7cb4b6bdb332efaf2
Ben Brooks <ben@bbrks.me>
2015-04-23 19:21:03 +0100

    Add horizontal rule between module questions and add module code to answer id

--- src/app/views/questionnaires/view.php
+++ src/app/views/questionnaires/view.php
@@ -44,12 +44,12 @@ global $lang;
     }
 
     foreach ($modules as $module) {
-      echo '<h3>'.$module['module_code'].' - '.$module['title'].'</h3>';
+      echo '<hr/><h3>'.$module['module_code'].' - '.$module['title'].'</h3>';
       foreach ($questions as $question) {
         if ($question['module'] === "0" || $question['module'] == $module['module_code']) {
           echo '<div class="form-group">';
-          echo '<label for="answer['.$question['id'].']">'.htmlspecialchars($question['text_'.$lang]).'</label>';
-          echo '<input name="answer['.$question['id'].']" id="answer['.$question['id'].']"" type="'.$question['type'].'" class="form-control" />';
+          echo '<label for="answer['.$question['id'].$module['module_code'].']">'.htmlspecialchars($question['text_'.$lang]).'</label>';
+          echo '<input name="answer['.$question['id'].$module['module_code'].']" id="answer['.$question['id'].$module['module_code'].']"" type="'.$question['type'].'" class="form-control" />';
           echo '</div>';
         }
       }
